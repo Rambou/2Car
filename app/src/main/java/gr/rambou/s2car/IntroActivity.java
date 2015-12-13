b1e426c1cc2877e16994e8c0507926fac05abdbb
Rambou <icsd10124@icsd.aegean.gr>
2015-12-13 04:00:58 +0200

    API <23 error with getColor fixed!

--- app/src/main/java/gr/rambou/s2car/IntroActivity.java
+++ app/src/main/java/gr/rambou/s2car/IntroActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 
 import com.github.paolorotolo.appintro.AppIntro2;
@@ -15,12 +16,12 @@ public class IntroActivity extends AppIntro2 {
     @Override
     public void init(Bundle savedInstanceState) {
         // Instead of fragments, you can also use our default slide
-        addSlide(AppIntroFragment.newInstance(getString(R.string.IntroTitle1), getString(R.string.IntroText1), R.drawable.ic_logo, getColor(R.color.intro1)));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.IntroTitle2), getString(R.string.IntroText2), R.drawable.ic_logo, getColor(R.color.intro2)));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.IntroTitle3), getString(R.string.IntroText3), R.drawable.kawasaki, getColor(R.color.intro3)));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.IntroTitle4), "Μοιράς ή βρες κι δίτρουχα \n", R.drawable.ic_logo, getColor(R.color.intro4)));
-        addSlide(AppIntroFragment.newInstance("Location", "One more permission! We need to locate your device. \n", R.drawable.ic_logo, getColor(R.color.intro5)));
-        addSlide(AppIntroFragment.newInstance("All set!", "All done! \n", R.drawable.ic_logo, getColor(R.color.intro6)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.IntroTitle1), getString(R.string.IntroText1), R.drawable.ic_logo, ContextCompat.getColor(getApplicationContext(), R.color.intro1)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.IntroTitle2), getString(R.string.IntroText2), R.drawable.ic_logo, ContextCompat.getColor(getApplicationContext(), R.color.intro2)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.IntroTitle3), getString(R.string.IntroText3), R.drawable.kawasaki, ContextCompat.getColor(getApplicationContext(), R.color.intro3)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.IntroTitle4), "Μοιράς ή βρες κι δίτρουχα \n", R.drawable.ic_logo, ContextCompat.getColor(getApplicationContext(), R.color.intro4)));
+        addSlide(AppIntroFragment.newInstance("Location", "One more permission! We need to locate your device. \n", R.drawable.ic_logo, ContextCompat.getColor(getApplicationContext(), R.color.intro5)));
+        addSlide(AppIntroFragment.newInstance("All set!", "All done! \n", R.drawable.ic_logo, ContextCompat.getColor(getApplicationContext(), R.color.intro6)));
 
         // Hide ActionBar and Statusbar
         ActionBar actionBar = getSupportActionBar();
