808fc382aa62dfb4a6e15fec49154c4415c35cbc
Armageddonas <icsd11175@icsd.aegean.gr>
2016-01-07 00:12:42 +0200

    Add advert creation for Vehicles  gui

--- app/src/main/java/gr/rambou/s2car/CreateAdActivity.java
+++ app/src/main/java/gr/rambou/s2car/CreateAdActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.parse.ParseUser;
 
@@ -24,6 +26,23 @@ public class CreateAdActivity extends AppCompatActivity {
 
         currentUser = ParseUser.getCurrentUser();
 
+        Spinner spinnerAdType = (Spinner) findViewById(R.id.SpnAdType);
+        ArrayAdapter<CharSequence> adapterAdType = ArrayAdapter.createFromResource(this,
+                R.array.AdType_array, R.layout.create_ad_spinner);
+        adapterAdType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAdType.setAdapter(adapterAdType);
+
+        Spinner spinnerBrand = (Spinner) findViewById(R.id.SpnBrand);
+        ArrayAdapter<CharSequence> adapterBrand = ArrayAdapter.createFromResource(this,
+                R.array.Brand_array, R.layout.create_ad_spinner);
+        adapterBrand.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerBrand.setAdapter(adapterBrand);
+
+        Spinner spinnerFuel = (Spinner) findViewById(R.id.SpnFuel);
+        ArrayAdapter<CharSequence> adapterFuel = ArrayAdapter.createFromResource(this,
+                R.array.Fuel_array, R.layout.create_ad_spinner);
+        adapterFuel.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFuel.setAdapter(adapterFuel);
     }
 
     @Override
