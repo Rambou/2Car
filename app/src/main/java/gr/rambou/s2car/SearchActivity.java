13afe0c663bb79cc21f00b4ed6b1b96a6c785b68
Rambou <icsd10124@icsd.aegean.gr>
2016-01-11 20:44:42 +0200

    Search bug fixed

--- app/src/main/java/gr/rambou/s2car/SearchActivity.java
+++ app/src/main/java/gr/rambou/s2car/SearchActivity.java
@@ -2,6 +2,8 @@ package gr.rambou.s2car;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Αναζήτηση");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
